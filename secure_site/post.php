3f682ec47a8492b7c20c0596e80c6ab1a1494400
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-05 12:48:18 +0700

    feat(post): check valid CSRF token

--- secure_site/post.php
+++ secure_site/post.php
@@ -9,7 +9,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 include 'db_config.php';
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    
+    if(!isset($_POST["csrf_token"]) || $_POST["csrf_token"] !== $_SESSION["csrf_token"]) {
+        die("CSRF token không hợp lệ.");
+    }
     if(empty(trim($_POST["content"]))){
         header("location: index.php");
         exit;
